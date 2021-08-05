100d59785464d25938d3fe53221e96a53984a652
Boubekeur <rachidboud>
2021-08-05 13:11:11 +0200

    :hammer: development script for Profile containers

---
+++ frontend/src/containers/Profile/index.jsx
@@ -0,0 +1,13 @@
+// import { WelcomeProfile } from '../../components/smart/WelcomeProfile';
+// import { Account } from '../../components/smart/Account';
+import './profile.css';
+
+export const Profile = () => {
+    return (
+        <section class="main bg-dark">
+            {/* <WelcomeProfile /> */}
+            <h2 class="sr-only">Accounts</h2>
+            {/* <Account /> */}
+        </section>
+    );
+};
