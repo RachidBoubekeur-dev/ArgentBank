3b5190b7d8b639a1ccb353a7b325397607fe9191
Boubekeur <rachidboud>
2021-08-05 13:21:33 +0200

    :hammer: add WelcomeProfile in Profile container

--- frontend/src/containers/Profile/index.jsx
+++ frontend/src/containers/Profile/index.jsx
@@ -1,11 +1,11 @@
-// import { WelcomeProfile } from '../../components/smart/WelcomeProfile';
+import { WelcomeProfile } from '../../components/smart/WelcomeProfile';
 // import { Account } from '../../components/smart/Account';
 import './profile.css';
 
 export const Profile = () => {
     return (
         <section class="main bg-dark">
-            {/* <WelcomeProfile /> */}
+            <WelcomeProfile />
             <h2 class="sr-only">Accounts</h2>
             {/* <Account /> */}
         </section>
