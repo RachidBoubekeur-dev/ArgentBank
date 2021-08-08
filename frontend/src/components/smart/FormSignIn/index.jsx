d950722a3fb2441a210c28ba1a181777c9807d9e
Boubekeur <rachidboud>
2021-08-08 07:53:22 +0200

    :hammer: development script add redux state

--- frontend/src/components/smart/FormSignIn/index.jsx
+++ frontend/src/components/smart/FormSignIn/index.jsx
@@ -1,3 +1,8 @@
+import { useState } from 'react';
+import Axios from '../../../services/Axios';
+import { useDispatch } from 'react-redux';
+import { profile } from '../../../slices/User';
+import { useHistory } from 'react-router-dom';
 import './formSignIn.css';
 
 /**
@@ -5,24 +10,80 @@ import './formSignIn.css';
  *  login form
  */
 export const FormSignIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [remember, setRemember] = useState(false);
+    const [error, setError] = useState(false);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    /**
+     *  SetUserData send data to Slice
+     * @param  {Object} userData this is the data received by Axios
+     */
+    const SetUserData = (userData) => {
+        dispatch(profile({ ...userData }));
+        remember &&
+            localStorage.setItem('user', JSON.stringify({ ...userData }));
+        history.push('/profile');
+    };
+
+    const SignInSubmit = (e) => {
+        e.preventDefault();
+        const dataForm = {
+            email: email,
+            password: password,
+        };
+        Axios('login', dataForm)
+            .then((userData) => {
+                SetUserData(userData);
+            })
+            .catch((err) => {
+                setError(true);
+            });
+    };
+
     return (
         <div className="sign-in-content">
+            {error && (
+                <span style={{ color: 'red' }}>
+                    Your username or password is incorrect !
+                </span>
+            )}
             <i className="fa fa-user-circle sign-in-icon"></i>
             <h1>Sign In</h1>
-            <form>
+            <form onSubmit={SignInSubmit}>
                 <div className="input-wrapper">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" />
+                    <label htmlFor="username">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className="input-wrapper">
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" />
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className="input-remember">
-                    <input type="checkbox" id="remember-me" />
+                    <input
+                        type="checkbox"
+                        id="remember-me"
+                        onChange={(e) => setRemember(e.target.checked)}
+                    />
                     <label htmlFor="remember-me">Remember me</label>
                 </div>
-                <button className="sign-in-button">Sign In</button>
+                <input
+                    type="submit"
+                    value="Sign In"
+                    className="sign-in-button"
+                />
             </form>
         </div>
     );
