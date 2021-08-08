ab63f829e792daf414d96e21ebf8fa3527f8e922
Boubekeur <rachidboud>
2021-08-08 07:58:01 +0200

    :hammer: development script delete react-query

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const queryClient = new QueryClient();
-
 ReactDOM.render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </QueryClientProvider>
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
